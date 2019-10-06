a83d895c8558af3cb9061d04762a0e866e90a058
Tomato <gamerx6900@gmail.com>
2019-10-06 20:28:41 +0300

    small button fix 6/10/2019 sunday 8:28pm

--- app/src/main/java/com/example/hsport/fixtopia091/MainActivity.java
+++ app/src/main/java/com/example/hsport/fixtopia091/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.hsport.fixtopia091;
 
+import android.app.SearchManager;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button bFirst;
+    private Button BSec;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButtonSetting();
+        ButtonNext();
+    }
+
+    private void ButtonSetting(){
+        bFirst = findViewById(R.id.ButOne);
+        BSec = findViewById(R.id.ButTwo);
+    }
+
+    private void ButtonNext(){
+        bFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SelectPhone.class);
+                        startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hsport/fixtopia091/SelectPhone.java
@@ -0,0 +1,13 @@
+package com.example.hsport.fixtopia091;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_phone);
+    }
+}
