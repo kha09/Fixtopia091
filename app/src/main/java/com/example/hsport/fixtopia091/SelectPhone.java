41ebb6b5de6b729a318edf40ef9973e78a5f479b
Tomato <gamerx6900@gmail.com>
2019-10-08 20:21:27 +0300

    CardView Sec Layout 8/10/2019 Tuesday 8:21 pm

--- app/src/main/java/com/example/hsport/fixtopia091/SelectPhone.java
+++ app/src/main/java/com/example/hsport/fixtopia091/SelectPhone.java
@@ -1,13 +1,23 @@
 package com.example.hsport.fixtopia091;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 
 public class SelectPhone extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_phone);
+        setUpToolbar();
     }
-}
+
+    private void setUpToolbar(){
+        toolbar = findViewById(R.id.toolBar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
